faba5811660a5214d4c97408fe95250eaaf349a4
Martyn Bissett <martynbissett@yahoo.co.uk>
2016-05-27 12:06:36 +0900

    Add form

--- module/Article/src/Article/Controller/ArticleController.php
+++ module/Article/src/Article/Controller/ArticleController.php
@@ -4,6 +4,8 @@ namespace Article\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Article\Form\ArticleForm;
+use Article\Model\Article;
 
 class ArticleController extends AbstractActionController
 {
@@ -18,6 +20,25 @@ class ArticleController extends AbstractActionController
 
     public function addAction()
     {
+        $form = new ArticleForm();
+        $form->get('submit')->setValue('Add');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $article = new Article();
+            $form->setInputFilter($article->getInputFilter());
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $article->exchangeArray($form->getData());
+                $this->getArticleTable()->saveArticle($article);
+
+                // Redirect to list of articles
+                return $this->redirect()->toRoute('article');
+            }
+        }
+        return array('form' => $form);
+
     }
 
     public function editAction()
